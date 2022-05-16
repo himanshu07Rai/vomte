a30ad7cced0ac144532c502a68a2e6bfee07663d
Himanshu Rai <raihimanshu22345gmail.com>
2022-05-16 15:53:42 +0530

    error handling

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require("express");
+const cors = require("cors");
+const createError = require("http-errors");
 
 const app = express();
 
+app.use(express.json());
+
+app.use(cors());
+
 app.get("/api", (req, res) => {
-  res.status(200).json({ message: "gg" });
+  res.status(200).send("🚀 Server running 🚀");
 });
 
 app.use("/api/dashboard", require("./routes/dashboard"));
 app.use("/api/auth", require("./routes/auth"));
 
-app.listen(5000, () => {
-  console.log("bitch");
+app.use((req, res, next) => {
+  next(createError(404, "Not Found"));
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
 });
